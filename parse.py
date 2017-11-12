82f666c244cdb99c051bdc5e24f7f151730579b0
Ryan Stephens <rstephens96@gmail.com>
2017-11-12 17:56:57 -0500

    Emoji test cases

--- parse.py
+++ parse.py
@@ -1,4 +1,6 @@
 from lark import Lark
+import sys
+import convert_code
 
 RULES = '''
 start: procedure start -> start0 
@@ -19,7 +21,7 @@ stars: "" -> stars0
 dcls: "" -> dcls0
     | dcls? BABY dcl BECOMES NUM SEMI -> dcls1
     | dcls? BABY dcl BECOMES NULL SEMI -> dcls2
-    | dcls? BABY dcl BECOMES "\'" CHARACTER "\'" -> dcls3
+    | dcls? BABY dcl BECOMES "'" CHARACTER "'" SEMI -> dcls3
 dcl: type WORD
 statements: "" -> statements0
     | statements? statement -> statements1
@@ -48,9 +50,9 @@ factor: WORD -> factor0
     | REF lvalue -> factor4
     | DEREF factor -> factor5
     | NEW type LBRACK expr RBRACK -> factor6
-    | CALL WORD LPAREN RPAREN -> factor7
-    | CALL WORD LPAREN arglist RPAREN -> factor8
-    | CHARACTER -> factor9
+    | CALL WORD CLAP CLAP -> factor7
+    | CALL WORD CLAP arglist CLAP -> factor8
+    | "'" CHARACTER "'" -> factor9
 arglist: expr -> arglist0
     | expr CLAP arglist -> arglist1
 lvalue: WORD -> lvalue0
@@ -58,70 +60,99 @@ lvalue: WORD -> lvalue0
     | LPAREN lvalue RPAREN -> lvalue2
 NUM: /[0-9]+/
 LONG: /[0-9]+/
-CHARACTER: /^[a-zA-Z]$/
-CHAR: "__char__"
+CHARACTER: /[a-zA-Z]/
+CHAR: "_char_"
 LPAREN: "("
 RPAREN: ")"
 LBRACE: "{"
 RBRACE: "}"
-RETURN: "__return__"
-HMMM: "__hmmm__"
-ELSE: "__else__"
-WHILE: "__while__"
-PRINTLN: "__println__"
-HOME: "__home__"
+RETURN: "_return_"
+HMMM: "_hmmm_"
+ELSE: "_else_"
+WHILE: "_while_"
+PRINTLN: "_println_"
+HOME: "_home_"
 BECOMES: "="
-INT: "__int__"
-TRUE: "__true__"
-FALSE: "__false__"
-BOOL: "__bool__"
+INT: "_int_"
+TRUE: "_true_"
+FALSE: "_false_"
+BOOL: "_bool_"
 EQ: "=="
 NE: "!="
 LT: "<"
 GT: ">"
 LE: "<="
 GE: ">="
-PLUS: "__plus__"
-MINUS: "__minus__"
-STAR: "__star__"
-SLASH: "__slash__"
+PLUS: "_plus_"
+MINUS: "_minus_"
+STAR: "_star_"
+SLASH: "_slash_"
 PCT: "%"
-CALL: "__call__"
-CLAP: "__clap__"
-BABY: "__baby__"
-SEMI: "__semi__"
-NEW: "__new__"
-DELETE: "__delete__"
-REF: "__ref__"
-DEREF: "__deref__"
+CALL: "_call_"
+CLAP: "_clap_"
+BABY: "_baby_"
+SEMI: "_semi_"
+NEW: "_new_"
+DELETE: "_delete_"
+REF: "_ref_"
+DEREF: "_deref_"
 LBRACK: "["
 RBRACK: "]"
 NULL: "NULL"
 %import common.WORD
 %ignore " "
 '''
-emoji_table = {
 
+
+
+# emoji unicode to 
+emoji_table = {
+    u'\U0001f170': '__char__',
+    u'\U0001F51A': '__return__',
+    u'\U0001F914': '__hmmm__',
+    u'\U0001F937': '__else__',
+    u'\U000023F1': '__while__',
+    u'\U0001F5A8': '__println__',
+    u'\U0001F3E0': '__home__', 
+    u'\U0001F4AF': '__int__', 
+    u'\U0001f44d': '__true__', 
+    u'\U0001f44e': '__false__', 
+    u'\U0001f171': '__bool__',
+    u'\U00002795': '__plus__', 
+    u'\U00002796': '__minus__', 
+    u'\U00002716': '__star__', 
+    u'\U00002797':'__slash__', 
+    u'\U0001f4f2': '__call__', 
+    u'\U0001f44f': '__clap__', 
+    u'\U0001f476': '__baby__', 
+    u'\U0001f44c': '__semi__', 
+    u'\U0001f195': '__new__', 
+    u'\U0001f5d1': '__delete__', 
+    u'\U0001f448': '__ref__', 
+    u'\U0001f449': '__deref__'
 }
 
 # Returns a parse-tree from a Cmoji file
 def parse_file(file_name):
     larker = Lark(RULES)
-    with open(filename, 'r') as f:
-        
+    parse_string = ''
+    with open(file_name, 'r', ) as f:
+        for line in f.read().split('\n'):
+            parse_string += line
+
+    # Pre-processes the cmoji file, replacing emojis with their parse equivelants
+    for emoji in emoji_table:
+        parse_string = parse_string.replace(emoji, emoji_table[emoji])
+
+    parse_string = parse_string.replace('\t', ' ').replace('\n', ' ')
     try:
-        tree = larker.parse()
+        tree = larker.parse(parse_string)
     except Exception:
+        print(parse_string)
         print('Needs more emoji\'s, mate')
         raise Exception
+        
     return tree
 
-test_case = '''
-__int__ __home__ __clap__ __int__ test __clap__ __char__ __deref__ __deref__ abc __clap__ 
-{ 
-    __baby__ __bool__ abc = 69 __semi__ 
-    __delete__ [] 6 __plus__ 9 __semi__
-    __return__ 0 __semi__ 
-}
-'''
-
+if len(sys.argv) > 1:
+    convert_code.convert(parse_file(sys.argv[1]))
